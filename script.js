306fbaa56b7f68b7a033b923eae9e31a8ca68972
dmellin <dmellin@kth.se>
2020-07-03 18:44:27 +0200

    Added internal attack paths in assets

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ var width = svg.attr("width")
 var height = svg.attr("height");
 var graph = {}
 
+var boxWidth = 160
+
 var colors = [
 //	[Dark shade, light shade]
 	["#D3367D", "#E072A4"],
@@ -12,37 +14,60 @@ var colors = [
 	["#553A49", "#9C6D87"]
 ]
 
+var allAttackSteps = [
+	{name: "| access", parent: "Network", index: 0, children: [0]},
+	{name: "| connect", parent: "Host", index: 0, children: [1]},
+	{name: "| authenticate", parent: "Host", index: 1, children: [2]},
+	{name: "| guessPassword", parent: "Host", index: 2, children: [3]},
+	{name: "| guessedPassword", parent: "Host", index: 3, children: [4]},
+	{name: "& access", parent: "Host", index: 4, children: []},
+	{name: "| obtain", parent: "Password", index: 0, children: [5]},
+	{name: "| attemptPhishing", parent: "User", index: 0, children: [7]},
+	{name: "| phish", parent: "User", index: 1, children: [6]}
+]
+
+var relations = [
+	{source: allAttackSteps[0], target: allAttackSteps[1]},
+	{source: allAttackSteps[1], target: allAttackSteps[5]},
+	{source: allAttackSteps[2], target: allAttackSteps[5]},
+	{source: allAttackSteps[3], target: allAttackSteps[4]},
+	{source: allAttackSteps[4], target: allAttackSteps[2]},
+	{source: allAttackSteps[6], target: allAttackSteps[2]},
+	{source: allAttackSteps[8], target: allAttackSteps[6]},
+	{source: allAttackSteps[7], target: allAttackSteps[8]},
+]
+
 var assets = [
 	{
 		name: "Network",
 		attackSteps: [
-			{name: "| access"}
+			allAttackSteps[0]
 		],
 		color: 0
 	},
 	{
 		name: "Host",
 		attackSteps: [
-			{name: "| connect"},
-			{name: "| authenticate"},
-			{name: "| guessPassword"},
-			{name: "| guessedPassword"},
-			{name: "& access"}
+			allAttackSteps[1],
+			allAttackSteps[2],
+			allAttackSteps[3],
+			allAttackSteps[4],
+			allAttackSteps[5]
 		],
 		color: 1
 	},
 	{
 		name: "Password",
 		attackSteps: [
-			{name: "| obtain"}
+			allAttackSteps[6]
 		],
 		color: 2
 	},
 	{
 		name: "User",
 		attackSteps: [
-			{name: "| attemptPhishing"},
-			{name: "| phish"}
+			allAttackSteps[7],
+			allAttackSteps[8]
 		],
 		color: 3
 	}
@@ -78,23 +103,23 @@ graph.assetBox = graph.asset.append(createAssetBox)
 function ticked() {
 	//Update Association link position
 	graph.associationLink.attr('stroke-width', 2)
-		.style('stroke', 'black')
+		.style('stroke', 'gray')
 		.attr('x1', function(d) {
-		return d.source.x + 100
+			return d.source.x
 		})
 		.attr('y1', function(d) {
-		return d.source.y + (30 * d.sdata.attackSteps.length + 40)/2
+			return d.source.y + (30 * d.sdata.attackSteps.length + 40)/2
 		})
 		.attr('x2', function(d) {
-		return d.target.x + 100
+			return d.target.x
 		})
 		.attr('y2', function(d) {
-		return d.target.y + (30 * d.tdata.attackSteps.length + 40)/2
+			return d.target.y + (30 * d.tdata.attackSteps.length + 40)/2
 		})
 
 	//Update Asset position
 	graph.asset.attr('transform', function(d) {
-		return 'translate(' + d.x + ',' + d.y + ')';
+		return 'translate(' + (d.x - boxWidth/2) + ',' + d.y + ')';
 	});
 }
 
@@ -102,11 +127,15 @@ function ticked() {
 function createAssetBox(d) {
 	var group = document.createElementNS('http://www.w3.org/2000/svg', 'g')
 
+	var labelHeight = 40
+	var attackStepHeight = 30
+	var sideMargin = 5
+
 	//Boundning rectangle
 	var rect = document.createElementNS('http://www.w3.org/2000/svg', 'rect')
 	rect.setAttributeNS(null, 'fill', colors[d.color][0])
-	rect.setAttributeNS(null, 'width', 200)
-	rect.setAttributeNS(null, 'height', 30 * d.attackSteps.length + 40)
+	rect.setAttributeNS(null, 'width', boxWidth)
+	rect.setAttributeNS(null, 'height', attackStepHeight * d.attackSteps.length + labelHeight)
 	rect.setAttributeNS(null, 'rx', 5)
 	rect.setAttributeNS(null, 'ry', 5)
 	group.appendChild(rect)
@@ -115,7 +144,7 @@ function createAssetBox(d) {
 	var label = document.createElementNS('http://www.w3.org/2000/svg', 'text')
 	label.textContent = d.name;
 	label.setAttributeNS(null, 'font-size', '1.25em')
-	label.setAttributeNS(null, 'x', 100)
+	label.setAttributeNS(null, 'x', boxWidth/2)
 	label.setAttributeNS(null, 'y', 25)
 	label.setAttributeNS(null, 'text-anchor', 'middle')
 	label.setAttributeNS(null, 'font-family', 'Arial')
@@ -123,24 +152,56 @@ function createAssetBox(d) {
 	group.appendChild(label)
 
 	for(step in d.attackSteps) {
-		attackStep = d.attackSteps[step]
+		var attackStep = d.attackSteps[step]
 		//Rectangle for each Attack Step
 		var asbox = document.createElementNS('http://www.w3.org/2000/svg', 'rect')
 		asbox.setAttributeNS(null, 'fill', colors[d.color][1])
-		asbox.setAttributeNS(null, 'x', 5)
-		asbox.setAttributeNS(null, 'y', step * 30 + 40)
-		asbox.setAttributeNS(null, 'width', 190)
-		asbox.setAttributeNS(null, 'height', 25)
+		asbox.setAttributeNS(null, 'x', sideMargin)
+		asbox.setAttributeNS(null, 'y', step * attackStepHeight + labelHeight)
+		asbox.setAttributeNS(null, 'width', boxWidth - sideMargin * 2)
+		asbox.setAttributeNS(null, 'height', attackStepHeight - 5)
 		group.append(asbox)
 		//Name of each Attack Step
 		var text = document.createElementNS('http://www.w3.org/2000/svg', 'text')
 		text.textContent = attackStep.name;
-		text.setAttributeNS(null, 'x', 100)
-		text.setAttributeNS(null, 'y', step * 30 + 57)
+		text.setAttributeNS(null, 'x', boxWidth/2)
+		text.setAttributeNS(null, 'y', step * attackStepHeight + labelHeight + 17)
 		text.setAttributeNS(null, 'text-anchor', 'middle')
 		text.setAttributeNS(null, 'font-family', 'Arial')
 		text.setAttributeNS(null, 'fill', 'black')
 		group.appendChild(text)
 	}
+
+	//Draw internal Attack paths
+	for(step in d.attackSteps) {
+		var attackStep = d.attackSteps[step]
+		for(child in attackStep.children) {
+			relation = relations[attackStep.children[child]]
+			if(relation.source.parent == relation.target.parent) {
+				var line = document.createElementNS('http://www.w3.org/2000/svg', 'path')
+				var ys = (attackStep.index * attackStepHeight + labelHeight + 12)
+				var yt = (relation.target.index * attackStepHeight + labelHeight + 12)
+				var bend = 10
+				if(relation.source.index < relation.target.index) {
+					var start = "M " + boxWidth + " " + ys + " "
+					var c1 = "" + (boxWidth + 15 + (bend*Math.abs(relation.source.index - relation.target.index))) + " " + ys
+					var c2 = "" + (boxWidth + 15 + (bend*Math.abs(relation.source.index - relation.target.index))) + " " + yt
+					var end = (boxWidth + 5) + " " + yt
+				} else {
+					var start = "M " + 0 + " " + ys + " "
+					var c1 = "" + (0 - 15 - (bend*Math.abs(relation.source.index - relation.target.index))) + " " + ys
+					var c2 = "" + (0 - 15 - (bend*Math.abs(relation.source.index - relation.target.index))) + " " + yt
+					var end = -5 + " " + yt
+				}
+				line.setAttributeNS(null, 'd', start + " C " + c1 + " " + c2 + " " + end)
+				line.setAttributeNS(null, 'stroke-width', 1.1)
+				line.setAttributeNS(null, 'stroke', 'black')
+				line.setAttributeNS(null, 'fill', 'transparent')
+				line.setAttributeNS(null, 'marker-end', 'url(#arrow)')
+				group.appendChild(line)
+			}
+		}
+	}
+
 	return group
 }
